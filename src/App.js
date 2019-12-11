898a029a79be055a2de80605f5e00b1c2ad7a88f
huangli_jusda <huangli@jusda.int>
2019-12-11 16:11:13 +0800

    1.concurrently合并启动 2.增加路由 3.增加redux数据处理

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
+import {Route} from 'react-router-dom';
+import Index from './container/index';
+import About from './container/about';
 
 
 
-function App (props) {
-    const [count, setCount] = useState(1)
-    return <div>
-        <h1>哈罗 {props.title} ! {count}</h1>
-        <button onClick={() => setCount(count + 1)}>累加</button>
+
+
+export default (
+    <div>
+        <Route path='/' exact component={Index}></Route>
+        <Route path='/about' exact component={About}></Route>
     </div>
-}
-
-export default <App title="开课吧"></App>;
+)
---
+++ src/container/index.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import {getIndexList} from '../store/index'
+
+
+
+function Index (props) {
+    console.log('props==>',props)
+    const [count, setCount] = useState(1)
+    useEffect(() => {
+        // 异步数据 首页显示
+        props.getIndexList()
+    }, [])
+    return <div>
+        <h1>哈罗 {props.title} ! {count}</h1>
+        <button onClick={() => setCount(count + 1)}>累加</button>
+        <hr></hr>
+        <ul>
+            {props.list.map(item => {
+                return <li key={item.id}>{item.name}</li>
+            })}
+        </ul>
+    </div>
+}
+
+export default connect( 
+    state => ({list: state.index.list}),
+    {getIndexList}
+)(Index);
